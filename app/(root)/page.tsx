76938c80e57af9891b98a59813f464407d565e93
Alex <melnykoleksandr547@gmail.com>
2025-07-02 16:01:48 +0200

    Imlement the complete Layout

---
+++ app/(root)/page.tsx
@@ -0,0 +1,36 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
+import React from 'react'
+
+const Home = () => {
+  const loggedIn = {firstName: 'Alex', lastName:'Mel',email:'contact@mastery.pro'};
+  return (
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+         <HeaderBox
+         type="greeting"
+         title="Welcome"
+         user={loggedIn?.firstName || 'Guest'}
+         subtext="Access and manage your account and transaction efficiently."
+         />
+         <TotalBalanceBox
+         accounts={[]}
+         totalBanks={1}
+         totalCurrentBalance={1250.35}
+         />
+        </header>
+        RECENT TRANSACTIONS
+
+      </div>
+      <RightSidebar 
+      user = {loggedIn}
+      transactions={[]}
+      banks={[{},{}]}
+      />
+    </section>
+  )
+}
+
+export default Home
